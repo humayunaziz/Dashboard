9d8d204d490411601a013b5babf8b020f55977b9
humayun <humayun.aziz@dhakarachi.org>
2022-06-03 00:35:16 +0500

    add jwt handling

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Depends
 import json
-
-from pydantic import constr
-import config
-import pyodbc
-import pandas as pd
-
 from dbutility import DbUtility
+from auth_handler import signJWT
+from auth_bearer import JWTBearer
+from model import PostSchema
 
 app = FastAPI()
+posts = []
 
 
 @app.get("/")
 def getData():
+    return 'done'
+
+
+@app.get("/{email}")
+def getData(email: str):
+    return signJWT(email)
+
+
+@app.post("/customers", dependencies=[Depends(JWTBearer())])
+def getcustomers():
     return DbUtility().getCustomer()
+
+
+@app.post("/posts", dependencies=[Depends(JWTBearer())], tags=["posts"])
+async def add_post(post: PostSchema) -> dict:
+    post.id = len(posts) + 1
+    posts.append(post.dict())
+    return posts
